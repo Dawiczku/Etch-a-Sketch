7e78dd918d8a6c2e2d62629e3e6bac5705dce850
Dawiczku <skarupski.dawid@gmail.com>
2022-12-03 11:40:33 +0100

    Get elements by id's instead of classes

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-let board = document.getElementsByClassName("board");
-let slider = document.getElementsByClassName("slider");
-let sliderDesc = decoument.getElementsByClassName("slider-desc");
+let board = document.getElementById("board");
+let slider = document.getElementById("slider");
+let sliderDesc = document.getElementById("slider-desc");
 let clearBtn = document.getElementById("clear");
 let lightBtn = document.getElementById("lightening");
 let shadeBtn = document.getElementById("shading");
 let rainbowBtn = document.getElementById("rainbow");
 let userClrBtn = document.getElementById("user-color");
 let colorPicker = document.getElementById("colorpicker");
+
+for(let i = 0; i < 32; i++){
+    let box = document.createElement("div");
+    box.className = "box";
+    board.appendChild(box);
+}
