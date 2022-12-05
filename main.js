c8cc51792ce80328af0181650d5ad02ee4c5b47b
Dawiczku <skarupski.dawid@gmail.com>
2022-12-05 22:16:31 +0100

    Declare all needed DOM elements

--- main.js
+++ main.js
@@ -6,6 +6,17 @@ let currentColor = DEFAULT_COLOR;
 let currentMode = DEFAULT_MODE;
 let currentSize = DEFAULT_SIZE;
 
+const userColorButton = document.getElementById("user-color");
+const warmColorButton = document.getElementById("warm-color");
+const coldColorButton = document.getElementById("cold-color");
+const eraserButton = document.getElementById("eraser");
+const clearButton = document.getElementById("clear");
+const sliderValue = document.getElementById("slider-desc");
+const board = document.getElementById("board");
+const buttons = document.getElementsByTagName("button");
+const slider = document.getElementById("slider");
+const colorpicker = document.getElementById("colorpicker");
+
 function setCurrentColor(newColor) {
     currentColor = newColor;
 }
